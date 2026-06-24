18769108ad9303ab8f91a4f739ae78201b87bb84
toansbtc <toansbtc201@gmail.com>
2026-06-24 10:14:51 +0700

    rewrite be path

--- function/myUrl.tsx
+++ function/myUrl.tsx
@@ -1,1 +1,2 @@
-export const myUrl = "https://my-doc-rho.vercel.app/henryNguyen";
+// export const myUrl = "https://my-doc-rho.vercel.app/henryNguyen";
+export const myUrl = "/henryNguyen";
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/henryNguyen/:path*',
+        destination: 'https://my-doc-rho.vercel.app/henryNguyen/:path*',
+      },
+    ]
+  },
   /* config options here */
 };
 
